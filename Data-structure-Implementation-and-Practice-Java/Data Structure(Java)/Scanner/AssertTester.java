8a8c924dfa67d14ef368f921dda306164f82c4a3
ruiwenhe-10 <ruiwenhe@usc.edu>
2020-09-20 22:15:30 -0700

    delete

--- Data-structure-Implementation-and-Practice-Java/Data Structure(Java)/Scanner/AssertTester.java
+++
@@ -1,8 +0,0 @@
-
-
-public class AssertTester {
-    public static void main(String[] args) {
-        Term checking_1 = new Term(1.0, -1); // tester program intentionally makes Term.java assertion fail
-        Term checking_2 = new Term(1.0, 1); // valid input to Term.java
-    }
-}
